b3a868a763e2a64e359bce526c630f896cddfcd6
Philipp eTraverse <philippe.traverse@gmail.com>
2016-01-29 22:44:23 -0800

    added moar shit

--- server.js
+++ server.js
@@ -6,8 +6,7 @@
 
   app = express();
 
-  app.use(express["static"](__dirname + '/app'));
-  app.use(express["static"](__dirname + '/bower_components'));
+  app.use(express["static"](__dirname ));
 
   app.listen("8080");
 
